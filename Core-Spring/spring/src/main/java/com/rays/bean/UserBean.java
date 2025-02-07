a77e34ad672f95343d998af884f39f9a8e73a99c
Your Name <meghapatidar9066@gmail.com>
2025-02-07 15:09:30 +0530

    done

---
+++ Core-Spring/spring/src/main/java/com/rays/bean/UserBean.java
@@ -0,0 +1,31 @@
+package com.rays.bean;
+
+public class UserBean {
+	
+	private String login = null;
+	private String password = null;
+//it is default constractor//
+	public UserBean() {
+	}
+//it is perameterised constractor
+	public UserBean(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+//it is setter method
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
